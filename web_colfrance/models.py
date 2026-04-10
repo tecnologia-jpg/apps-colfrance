60c1f9725e6997457960b7e9dad4c47d483562a7
AlexNocua <nocua68@gmail.com>
2026-04-10 11:40:16 -0500

    Ajustes en pagina web principal seccion de "Nosotros"

--- web_colfrance/models.py
+++ web_colfrance/models.py
@@ -18,3 +18,25 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    asunto = models.CharField(max_length=100)
+    mensaje = models.TextField()
+    fecha_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.asunto}"
+
+
+class vacantes(models.Model):
+    cargo = models.CharField(max_length=50)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    cedula = models.IntegerField()
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    cv = models.FileField()
+    fecha_envio = models.DateTimeField(auto_now_add=True)
