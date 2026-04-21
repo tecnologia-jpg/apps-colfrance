ce120beb835e98d69f68d88cf6d51a1c4264a43d
AlexNocua <nocua68@gmail.com>
2026-04-21 17:00:34 -0500

    Prueba de Hosts permitidos (General)

--- apps_colfrance/settings.py
+++ apps_colfrance/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["colf-app-service.azurewebsites.net", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Configuracion de logueo
 LOGIN_REDIRECT_URL = "aforo_silos:modulos"
