9d92bd7429d86ca17f07128d56dc3ab42a6f5f53
AlexNocua <nocua68@gmail.com>
2026-04-28 15:55:16 -0500

    Ajustes de debug

--- apps_colfrance/settings.py
+++ apps_colfrance/settings.py
@@ -31,7 +31,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
